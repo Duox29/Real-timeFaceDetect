7bd06a06a48d8aaa200d9a60ffd289bb3823b142
Duox29 <leduox29@gmail.com>
2023-11-17 21:48:05 +0700

    Test
    Try to merge testcmd1 and testcmd2

---
+++ Create data .py
@@ -0,0 +1,59 @@
+import cv2
+import os
+import time
+import numpy as np
+detector=cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_default.xml')
+
+print("Menu:")
+print("1. NHập ảnh từ file data")
+print("2. Nhập ảnh từ webcam")
+print("0. Thoát")
+id = input("Nhập lựa chọn của bạn: ").strip()
+
+# Nhập ảnh từ file data
+if id == '1':
+    print(0)
+    for i in range(1,6):
+        for j in range (1,21):
+            filename = 'data/anh.'  + str(i) + '.' +str(j) + '.jpg'
+            frame = cv2.imread(filename)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            fa = detector.detectMultiScale(gray, 1.1, 5)
+            for(x,y,w,h) in fa:
+                cv2.rectangle(frame,(x,y),(x+w, y+h),(0,255,0), 2)
+                if not os.path.exists('dataset'):
+                    os.makedirs('dataset')
+                cv2.imwrite('dataset/anh'  + str(i) + '.' +str(j) + '.jpg', gray[y:y+h,x:x+w])
+
+
+# Nhập ảnh từ webcam
+if id == '2':
+    nModel = int(input("Số lượng model: "))
+    nImg = int(input("Số lượng ảnh mỗi model:"))
+    with open("config.txt", "w") as file:
+        file.write(f"nModel = {nModel}\nnImg = {nImg}")
+
+    for i in range(1,nModel+1):
+        cap = cv2.VideoCapture(0)
+        detector = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        sampleNum = 0
+        while (True):
+            ret, frame = cap.read()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            fa = detector.detectMultiScale(gray, 1.1, 5)
+            for (x, y, w, h) in fa:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                if not os.path.exists('dataset'):
+                    os.makedirs('dataset')
+                sampleNum += 1
+                cv2.imwrite('dataset/anh' + str(5) + '.' + str(sampleNum) + '.jpg', gray[y:y + h, x:x + w])
+            cv2.imshow('frame', frame)
+            time.sleep(0.1)
+            print("Đã chụp thành công ảnh số ", sampleNum)
+            if sampleNum > nImg-1:
+                break;
+        prompt = input("Bạn có muốn tiếp tục(y/n): ");
+        if prompt == 'n':
+            break;
+    cap.release()
+    cv2.destroyAllWindows()
---
+++ train.py
@@ -0,0 +1,72 @@
+import cv2
+import numpy as np
+from PIL import Image
+data = [] 
+label = []
+
+with open("config.txt", "r") as file:
+    content = file.readlines()
+
+# Gán giá trị cho các biến
+variables = {}
+for line in content:
+    parts = line.strip().split("=")
+    if len(parts) == 2:
+        variable_name = parts[0].strip()
+        variable_value = parts[1].strip()
+        variables[variable_name] = int(variable_value)
+
+nModel = variables.get("nModel")
+nImg = variables.get("nImg")
+
+
+for j in range (1,nModel+1):
+  for i in range (1,nImg+1):
+    filename = './dataset/anh'+ str(j) + '.'  + str(i) + '.jpg'
+    Img = cv2.imread(filename)
+    Img = cv2.cvtColor(Img, cv2.COLOR_BGR2GRAY)
+    Img = cv2.resize(src=Img, dsize=(100,100))
+    Img = np.array(Img)
+    data.append(Img)
+    label.append(j-1)
+data1 = np.array(data)
+label = np.array(label)
+data1 = data1.reshape((500,100,100,1))
+X_train = data1/255
+from sklearn.preprocessing import LabelBinarizer
+lb = LabelBinarizer()
+trainY =lb.fit_transform(label)
+from tensorflow.keras.models import Model
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import concatenate
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import SGD
+Model = Sequential()
+shape = (100,100, 1)
+Model.add(Conv2D(32,(3,3),padding="same",input_shape=shape))
+Model.add(Activation("relu"))
+Model.add(Conv2D(32,(3,3), padding="same"))
+Model.add(Activation("relu"))
+Model.add(MaxPooling2D(pool_size=(2,2)))
+Model.add(Conv2D(64,(3,3), padding="same"))
+Model.add(Activation("relu"))
+Model.add(MaxPooling2D(pool_size=(2,2)))
+Model.add(Flatten())
+Model.add(Dense(512))
+Model.add(Activation("relu"))
+Model.add(Dense(5))
+Model.add(Activation("softmax"))
+Model.summary()
+Model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+print("start training")
+Model.fit(X_train,trainY,batch_size=5,epochs=10)
+Model.save("khuonmat.keras")
